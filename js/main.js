9b6f84d4ab3c79e3570407c214d90271f7eec0e2
aleung910 <aliceleung34@gmail.com>
2023-09-09 18:25:44 -0400

    Cleaned up CSS and HTML

--- js/main.js
+++ js/main.js
@@ -29,13 +29,16 @@ function getFetch(){
       .then(data => {
         console.log(data)
         if (data.media_type ==='image'){
+
+        // document.getElementById('.img').style.backgroundImage = `url('${data.hdurl}')`
+
         document.querySelector('img').src = data.hdurl
         }
         else if (data.media_type ==='video'){
           document.querySelector('iframe').src = data.url
         }
         document.querySelector('h2').innerHTML=data.title
-        document.querySelector('h3').innerText=data.explanation
+        document.querySelector('p').innerText=data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
